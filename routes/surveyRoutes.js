eb73fe1a550068442abb925adcfdee1ccdfc835d
さいとうはじめ <saitohajime@saitouhajimenoMacBook-Pro.local>
2020-12-27 11:57:42 +0900

    end

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,44 +1,39 @@
-const _ = require("lodash");
-const { Path } = require("path-parser");
-const { URL } = require("url");
-const mongoose = require("mongoose");
-const requireLogin = require("../middlewares/requireLogin");
-const requireCredits = require("../middlewares/requireCredits");
-const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
-const Survey = mongoose.model("surveys");
+const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get("/api/surveys", requireLogin, async (req, res) => {
-    const surveys = await Survey.find(
-      { _user: req.user.id },
-      { recipients: false },
-    );
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
 
     res.send(surveys);
   });
 
-  app.get("/api/surveys/:surveys/:choice", (req, res) => {
-    res.send("Thanks for voting!");
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
   });
 
-  app.post("/api/surveys/webhooks", (req, res) => {
-    const p = new Path("/api/surveys/:surveyId/:choice");
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
     _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
-          return {
-            email: email,
-            surveyId: match.surveyId,
-            choice: match.choice,
-          };
+          return { email, surveyId: match.surveyId, choice: match.choice };
         }
       })
       .compact()
-      .uniqBy("email", "surveyId")
+      .uniqBy('email', 'surveyId')
       .each(({ surveyId, email, choice }) => {
         Survey.updateOne(
           {
@@ -49,7 +44,7 @@ module.exports = (app) => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { "recipients.$.responded": true },
+            $set: { 'recipients.$.responded': true },
             lastResponded: new Date(),
           }
         ).exec();
@@ -59,7 +54,7 @@ module.exports = (app) => {
     res.send({});
   });
 
-  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -67,7 +62,7 @@ module.exports = (app) => {
       subject,
       body,
       recipients: recipients
-        .split(",")
+        .split(',')
         .map((email) => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now(),
@@ -81,6 +76,7 @@ module.exports = (app) => {
       await survey.save();
       req.user.credits -= 1;
       const user = await req.user.save();
+
       res.send(user);
     } catch (err) {
       res.status(422).send(err);
--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
-        this.from_email = new helper.Email('saito.hajime0112@gmail.com');
+        this.from_email = new helper.Email('no-replay@emaily.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients);
