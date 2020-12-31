c00b86f3d337a7a40fb914e05aa4aa7bde0b9c78
さいとうはじめ <saitohajime@saitouhajimenoMacBook-Pro.local>
2020-12-31 23:19:39 +0900

    fixed

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-require('./routes/billingRoute')(app);
+require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
